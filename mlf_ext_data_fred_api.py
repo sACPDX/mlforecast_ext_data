0e3801da30a12c766c1ef45d9035c9f38fbb9247
Soren Andersen <sorenpdx@gmail.com>
2020-09-05 14:15:22 -0700

    Commit remove high row count tables

--- mlf_ext_data_fred_api.py
+++ mlf_ext_data_fred_api.py
@@ -88,7 +88,10 @@ create_Fed_data_table(data_gdp, mlf_table_name)
 
 ##############################################################################################################
 #
-# CPI
+# CPI - CPIAUCSL
+#
+#   Size:  <1K (250)
+# 
 #
 ##############################################################################################################
 
@@ -104,6 +107,9 @@ create_Fed_data_table(data_CPIAUCSL, mlf_table_name)
 ##############################################################################################################
 #
 # T10YIE - 10-Year Breakeven Inflation Rate (T10YIE)
+#
+#   Size: 4.5K
+#
 # Suggested Citation:
 #   Federal Reserve Bank of St. Louis, 10-Year Breakeven Inflation Rate [T10YIE], retrieved from FRED, Federal Reserve Bank of St. Louis; https://fred.stlouisfed.org/series/T10YIE, September 4, 2020.
 #
@@ -115,12 +121,15 @@ st.write("10-Year Breakeven Inflation Rate (T10YIE)", data_T10YIE)
 mlf_table_name = "data_T10YIE"
 
 # Load data to table
-create_Fed_data_table(data_T10YIE, mlf_table_name)
+#create_Fed_data_table(data_T10YIE, mlf_table_name)
 
 
 ##############################################################################################################
 #
-# Unemployment Rate - US
+# Unemployment Rate - US - UNRATE
+#
+#   Size:  <1K
+# 
 #
 ##############################################################################################################
 
@@ -136,6 +145,8 @@ create_Fed_data_table(data_UNRATE, mlf_table_name)
 #
 # M2 Money Supply
 #
+#   Size:  2.1K
+#
 ##############################################################################################################
 
 data_M2 = data.series('M2')
@@ -144,12 +155,14 @@ st.write("M2 Money Supply", data_M2)
 mlf_table_name = "data_M2"
 
 # Load data to table
-create_Fed_data_table(data_M2, mlf_table_name)
+#create_Fed_data_table(data_M2, mlf_table_name)
 
 
 ##############################################################################################################
 #
 # DEXUSEU - US dollars for Euro - U.S. / Euro Foreign Exchange Rate
+#
+#   Size:  5.5K
 #
 ##############################################################################################################
 
@@ -159,12 +172,14 @@ st.write("DEXUSEU - US dollars for Euro - U.S. / Euro Foreign Exchange Rate", da
 mlf_table_name = "data_DEXUSEU"
 
 # Load data to table
-create_Fed_data_table(data_DEXUSEU, mlf_table_name)
+#create_Fed_data_table(data_DEXUSEU, mlf_table_name)
 
 
 ##############################################################################################################
 #
 # UMCSENT - University of Michigan: Consumer Sentiment
+#
+#   Size:  <1K
 # 
 # This data should be cited as follows: "Surveys of Consumers, University of Michigan, University of Michigan: Consumer Sentiment © [UMCSENT], retrieved from FRED, Federal Reserve Bank of St. Louis, (Accessed on date)"
 # Copyright, 2016, Surveys of Consumers, University of Michigan. Reprinted with permission.
@@ -185,6 +200,9 @@ create_Fed_data_table(data_UMCSENT, mlf_table_name)
 ##############################################################################################################
 #
 # DGS10 - 10-Year Treasury Constant Maturity Rate
+#
+#   Size:  15K
+#
 # Suggested Citation:
 #   Board of Governors of the Federal Reserve System (US), 10-Year Treasury Constant Maturity Rate [DGS10], retrieved from FRED, Federal Reserve Bank of St. Louis; https://fred.stlouisfed.org/series/DGS10, September 4, 2020.
 #
@@ -196,12 +214,15 @@ st.write("DGS10 - 10-Year Treasury Constant Maturity Rate", data_DGS10)
 mlf_table_name = "data_DGS10"
 
 # Load data to table
-create_Fed_data_table(data_DGS10, mlf_table_name)
+#create_Fed_data_table(data_DGS10, mlf_table_name)
 
 
 ##############################################################################################################
 #
 # S&P 500 (SP500)
+#
+#   Size: 2.5K
+#
 # Suggested Citation:
 #   S&P Dow Jones Indices LLC, S&P 500 [SP500], retrieved from FRED, Federal Reserve Bank of St. Louis; https://fred.stlouisfed.org/series/SP500, September 4, 2020.
 #
@@ -219,6 +240,9 @@ create_Fed_data_table(data_SP500, mlf_table_name)
 ##############################################################################################################
 #
 # Median Sales Price of Houses Sold for the United States (MSPUS)
+#
+#   Size: <1K
+#
 # Suggested Citation:
 #   U.S. Census Bureau and U.S. Department of Housing and Urban Development, Median Sales Price of Houses Sold for the United States [MSPUS], retrieved from FRED, Federal Reserve Bank of St. Louis; https://fred.stlouisfed.org/series/MSPUS, September 4, 2020.
 #
@@ -237,6 +261,8 @@ create_Fed_data_table(data_MSPUS, mlf_table_name)
 #
 # Gold Fixing Price 10:30 A.M. (London time) in London Bullion Market, based in U.S. Dollars (GOLDAMGBD228NLBM)
 #
+#   Size:  13.5K
+#
 # Suggested Citation:
 #   ICE Benchmark Administration Limited (IBA), Gold Fixing Price 10:30 A.M. (London time) in London Bullion Market, based in U.S. Dollars [GOLDAMGBD228NLBM], retrieved from FRED, Federal Reserve Bank of St. Louis; https://fred.stlouisfed.org/series/GOLDAMGBD228NLBM, September 4, 2020.#
 #
@@ -248,7 +274,7 @@ st.write("Gold Fixing Price 10:30 A.M. (London time) in London Bullion Market, b
 mlf_table_name = "data_GOLDAMGBD228NLBM"
 
 # Load data to table
-create_Fed_data_table(data_GOLDAMGBD228NLBM, mlf_table_name)
+#create_Fed_data_table(data_GOLDAMGBD228NLBM, mlf_table_name)
 
 
 # End
